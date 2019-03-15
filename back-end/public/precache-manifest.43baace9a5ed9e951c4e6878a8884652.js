cf904a584ef19b3bd5ec2dc3e02c438ab9c91428
School <school@Mateo.local>
2019-03-15 15:21:49 -0400

    build

---
+++ back-end/public/precache-manifest.43baace9a5ed9e951c4e6878a8884652.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "98623513832510c0e8d1",
+    "url": "/static/js/main.98623513.chunk.js"
+  },
+  {
+    "revision": "c0b1230814e30f8e4c48",
+    "url": "/static/js/2.c0b12308.chunk.js"
+  },
+  {
+    "revision": "98623513832510c0e8d1",
+    "url": "/static/css/main.aa702de1.chunk.css"
+  },
+  {
+    "revision": "c0b1230814e30f8e4c48",
+    "url": "/static/css/2.452aea89.chunk.css"
+  },
+  {
+    "revision": "eff40570bd6ca055266f1b41616e0721",
+    "url": "/index.html"
+  }
+];
